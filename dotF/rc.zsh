d2e0ffbb86ed269500b63c37a9d2dfdc8b3f598c
sisrfeng <liuweifengemail@163.com>
2022-01-16 15:28:47 +0800

    我是commit名__01月16日15:28

--- dotF/rc.zsh
+++ dotF/rc.zsh
@@ -174,10 +174,11 @@ select-word-style bash  # 斜杠 下划线等 会作为单词的分隔
 # [[==================================zsh插件管理：zplug=================================
 # 别用等号对齐! zsh的空格是有意义的!
 # zplug的环境变量都在这里指定了
+export ZPLUG_CACHE_DIR=$HOME/.cache
+
 export ZPLUG_HOME=$HOME/dotF/zplug
 export Z_P=$ZPLUG_HOME
 export ZPLUG_BIN=$Z_P/bin
-export ZPLUG_CACHE_DIR=$Z_P/.cache
 export ZPLUG_LOADFILE=$Z_P/packages.zsh  # todo: 把下面zplu '某人/某项目'扔到这文件
 export ZPLUG_REPOS=$Z_P/repos  # 默认是在$HOME/.zplug底下
 source $Z_P/init.zsh
